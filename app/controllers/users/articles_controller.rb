9498da7036fb90714e7a7add0db85708072bdb92
南波留翔太 <nabarushouta@MacBook-Pro.local>
2022-10-23 23:33:52 +0900

    記事一覧作成中

--- app/controllers/users/articles_controller.rb
+++ app/controllers/users/articles_controller.rb
@@ -3,6 +3,49 @@ class Users::ArticlesController < ApplicationController
   before_action :set_article, only:[:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = current_user.articles
+  end
+
+  def new
+    @article = current_user.articles.new
+  end
+
+  def create
+    @article = current_user.articles.new(article_params)
+    if @article.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to users_articles_url
+    else
+      flash[:danger] = '新規作成に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:success] = "投稿を編集しました。"
+      redirect_to users_articles_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:success] = "投稿を削除しました。"
+    redirect_to users_articles_url
+  end
+
+  private
+
+  def set_article
+    @article = current_user.articles.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :img)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'articles/index'
-  end
-  devise_for :users
   root 'users/dash_bords#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
       # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   namespace :users do
     get 'dash_bords/index'
-    resources :articles 
+    resources :articles
   end
 end
